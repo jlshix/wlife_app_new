eb2c260f307f72780821068ec70608ab987af0e9
jlshix <jlshix@163.com>
2016-07-19 22:44:02 +0800

    新增右滑返回

--- app/src/main/java/com/jlshix/wlife_v03/activity/SettingsActivity.java
+++ app/src/main/java/com/jlshix/wlife_v03/activity/SettingsActivity.java
@@ -1,22 +1,39 @@
 package com.jlshix.wlife_v03.activity;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 import com.jlshix.wlife_v03.R;
 import com.jlshix.wlife_v03.fragment.SettingsFrag;
 import com.jlshix.wlife_v03.tool.BaseActivity;
+import com.jlshix.wlife_v03.tool.L;
 
 import org.xutils.view.annotation.ContentView;
 
 @ContentView(R.layout.activity_settings)
 public class SettingsActivity extends BaseActivity {
+
+    private GestureDetector gd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // 放置fragment
         getFragmentManager().beginTransaction().replace(R.id.container, new SettingsFrag()).commit();
+        // 手势处理 只看 onFling
+        gd = new GestureDetector(this, L.getSwipeBackListener(SettingsActivity.this));
+    }
 
+    /**
+     * 点击事件处理移交给 gs
+     * @param event 事件
+     * @return boolean
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return gd.onTouchEvent(event);
     }
 
 }
