a4661c101d91c1abdd8de7592f767f25cec6a2d2
jlshix <jlshix@163.com>
2016-07-24 00:52:43 +0800

    新增测试用的Activity

--- app/src/main/java/com/jlshix/wlife_v03/activity/MainActivity.java
+++ app/src/main/java/com/jlshix/wlife_v03/activity/MainActivity.java
@@ -460,6 +460,14 @@ public class MainActivity extends BaseActivity implements SwipeRefreshLayout.OnR
             return true;
         }
 
+        if (id == R.id.action_test) {
+            Intent intent = new Intent(getApplicationContext(), TestActivity.class);
+            startActivityForResult(intent, L.SETTINGS_REQUEST);
+            return true;
+        }
+
+
+
         return super.onOptionsItemSelected(item);
     }
 
